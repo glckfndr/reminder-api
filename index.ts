90191f0954f9a051a72e49db65528a35da0845ec
glckfndr <obulan65@gmail.com>
2024-03-22 11:06:51 +0200

    feat: add remindersRouter

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 import express from "express";
-
+import remindersRouter from "./routers/reminders";
 const app = express();
+
+app.use(express.json());
+app.use("/reminders", remindersRouter);
+
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
