69da70d16c1497306d74e0ea4e38ba1e5788e829
glckfndr <obulan65@gmail.com>
2024-03-22 12:32:02 +0200

    feat: add delete for remindersRouter

--- routers/reminders.ts
+++ routers/reminders.ts
@@ -1,15 +1,36 @@
 import { Router } from "express";
 import CreateReminderDto from "../dtos/create-reminder";
+import Reminder from "../models/reminder";
 
 const router = Router();
+const reminders: Reminder[] = [];
 
 router.get("/", (req, res) => {
-  res.send("List of reminders");
+  res.json(reminders);
+});
+
+router.get("/:id", (req, res) => {
+  const index = parseInt(req.params.id);
+  if (index < reminders.length) res.status(200).json(reminders[index]);
+  else res.status(400).json({ error: "not found" });
+});
+
+router.delete("/:id", (req, res) => {
+  const index = parseInt(req.params.id);
+  const el = reminders.find((el) => el.id === index);
+  if (el) {
+    const ind = reminders.indexOf(el);
+    reminders.splice(ind, 1);
+    res.json(reminders);
+  } else {
+    res.status(400).json({ error: "Deleted object is not found" });
+  }
 });
 
 router.post("/", (req, res) => {
   const { title } = req.body as CreateReminderDto;
-  res.json(title);
+  reminders.push(new Reminder(title));
+  res.status(201).json(reminders[reminders.length - 1]);
 });
 
 export default router;
